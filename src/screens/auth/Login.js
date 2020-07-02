963c22a0e7736c316c109d7ef10de8fe888b06d1
Mike Daley <michaelcdaley@yahoo.com>
2020-07-02 08:01:01 -0700

    Implemented sign-in with react-hook-forms and yup validation

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,41 +1,51 @@
 //-----------------------------------------------------------------------------
 // src/screens/auth/Login.js
 //-----------------------------------------------------------------------------
-import React, { useState }            from 'react'
+import React                    from 'react'
+import { useForm, Controller }  from 'react-hook-form'
+import { yupResolver }          from '@hookform/resolvers'
+import * as yup                 from 'yup'
 import {
   Keyboard,
   View,
-}                       from 'react-native'
+}                               from 'react-native'
 import {
   Button,
   Icon,
   Input,
   Text,
-}                       from 'react-native-elements'
+}                               from 'react-native-elements'
 
-import { AuthContext }  from '../context/AuthContext'
+import { AuthContext }          from '../context/AuthContext'
+
+// Define form validation schema
+const schema = yup.object().shape({
+  email:  
+    yup.string()
+      .email('Must be a valid email address')
+      .max(128, 'Email must be 128 characters or less')
+      .required('Email is require'),
+  password:
+    yup.string()
+      .max(128, 'Email must be 128 characters or less')
+      .required('Password is required')
+})
 
 /**
- * 
+ * Sign In screen
  * @param {*} param0 
  */
 const ScreensAuthLogin = ({navigation}) => {
-  const [email, setEmail]       = useState('')
-  const [password, setPassword] = useState('')
+  const { control, handleSubmit, errors } = useForm({resolver: yupResolver(schema)})
 
   const authContext = React.useContext(AuthContext)
 
-  const handleChangeEmail = (text) => {
-    setEmail(text)
-  }
-
-  const handleChangePassword = (text) => {
-    setPassword(text)
-  }
-
-  const handleSubmit = () => {
+  /**
+   * Attempt to sign in the user with the email and password.
+   * @param {Object} data - submitted form data, email & password
+   */
+  const onSubmit = ({email, password} = {}) => {
     Keyboard.dismiss()
-    //* alert(`Sign in: email=${email}, password=${password}`)
     authContext.signIn(email, password)
   }
 
@@ -49,47 +59,65 @@ const ScreensAuthLogin = ({navigation}) => {
           Login Screen
         </Text>
         <View style={{marginTop: 20, marginBottom: 20}}>
-          <Input
+          <Controller 
+            control = {control}
+            render  = { ({onChange, onBlur, value}) => (
+              <Input 
+                value         = {value}
+                placeholder   = 'Email address'
+                onBlur        = {onBlur}
+                onChangeText  = {(value) => onChange(value)}
+                leftIcon      = {
+                  <Icon
+                    type  = 'ionicon'
+                    name  = 'ios-mail'
+                    style = {{size: 24, paddingRight: 5}}
+                  />
+                }
+                keyboardType      = 'email-address'
+                textContentType   = 'emailAddress'
+                autoCapitalize    = 'none'
+                autoCompleteType  = 'email'
+                errorStyle        = {{ color: 'red' }}
+                errorMessage      = {errors.email ? errors.email.message : null}
+              />
+            )}
             name          = 'email'
-            value         = {email}
-            placeholder   = 'Email address'
-            onChangeText  = {handleChangeEmail}
-            leftIcon      = {
-              <Icon
-                type  = 'ionicon'
-                name  = 'ios-mail'
-                style = {{size: 24, paddingRight: 5}}
-              />
-            }
-            keyboardType      = 'email-address'
-            textContentType   = 'emailAddress'
-            autoCapitalize    = 'none'
-            autoCompleteType  = 'email'
-            errorStyle        = {{ color: 'red' }}
-            errorMessage      = {'Email is required'}
+            defaultValue  = ''
           />
-          <Input
+          
+          <Controller 
+            control = {control}
+            render  = { ({onChange, onBlur, value}) => (
+              <Input
+                value         = {value}
+                placeholder   = 'Password'
+                onBlur        = {onBlur}
+                onChangeText  = {(value) => onChange(value)}
+                leftIcon      = {
+                  <Icon 
+                    type  = 'ionicon'
+                    name  = 'ios-lock'
+                    style = {{size: 24, paddingRight: 10}}
+                  />
+                }
+                secureTextEntry   = {true}
+                keyboardType      = 'default'
+                textContentType   = 'password'
+                autoCapitalize    = 'none'
+                autoCompleteType  = 'password'
+                errorStyle        = {{ color: 'red' }}
+                errorMessage      = {errors.password ? errors.password.message : null}
+              />
+            )}
             name          = 'password'
-            value         = {password}
-            placeholder   = 'Password'
-            onChangeText  = {handleChangePassword}
-            leftIcon      = {
-              <Icon 
-                type  = 'ionicon'
-                name  = 'ios-lock'
-                style = {{size: 24, paddingRight: 10}}
-              />
-            }
-            secureTextEntry   = {true}
-            keyboardType      = 'default'
-            textContentType   = 'password'
-            autoCapitalize    = 'none'
-            autoCompleteType  = 'password'
+            defaultValue  = ''
           />
+          
           <Button 
             title   = 'Sign In'
             style   = {{padding: 5}}
-            onPress = {handleSubmit} 
+            onPress = {handleSubmit(onSubmit)} 
           />
         </View>
         <Button 
